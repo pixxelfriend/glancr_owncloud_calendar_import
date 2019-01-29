64effa40554d60bf850a40cac156d5c859f1223f
Steffen Hoffmann <steffen@twolabs.de>
2019-01-29 20:57:31 +0100

    remove duplicated code

--- backend/template.php
+++ backend/template.php
@@ -19,32 +19,13 @@ $base_url =  "http://" . $_SERVER["HTTP_HOST"] . "/modules/owncloud_calendar_imp
 ADD NEW CALENDAR
 
 <form id="oc_calendar_import">
-<?php
-	if(count($calendars) > 10){
-		foreach ($calendars as $key => $calendar){ ?>
-			<div class="oc_calendar_group">
-				<input type="text" class="oc_calendar_change" name="oc_calendar_name" placeholder="Bezeichnung" value="<?php echo $calendar->oc_calendar_name; ?>">
-				<input type="text" class="oc_calendar_change" name="oc_calendar_user" placeholder="Username" value="<?php echo $calendar->oc_calendar_user; ?>">
-				<input type="text" class="oc_calendar_change" name="oc_calendar_password" placeholder="Passwort" value="<?php echo $calendar->oc_calendar_password; ?>">
-				<input type="text" class="oc_calendar_change" name="oc_calendar_url" placeholder="url<?php echo _('oc_importer_url');?>" value="<?php echo $calendar->oc_calendar_url; ?>"/>
-				<p class="oc_calendar_export">
-					Copy2Cal: <span><?php echo $base_url . urlencode($calendar->oc_calendar_name);?></span>
-					<br />
-					<button class="oc_calendar__delete" type="button">
-						<span class="fi-trash"></span>
-						<?php echo _('oc_importer_delete');?>
-					</button>
-				</p>
-			</div>
-		<?php } 
-	}
-?>
-<div class="oc_calendar_group">
-	<input type="text" class="oc_calendar_change" name="oc_calendar_name" placeholder="Bezeichnung" value="">
-	<input type="text" class="oc_calendar_change" name="oc_calendar_user" placeholder="Username" value="">
-	<input type="text" class="oc_calendar_change" name="oc_calendar_password" placeholder="Passwort" value="">
-	<input type="text" class="oc_calendar_change" name="oc_calendar_url" placeholder="url<?php echo _('oc_importer_url');?>" value=""/>
-</div>
+
+	<div class="oc_calendar_group">
+		<input type="text" class="oc_calendar_change" name="oc_calendar_name" placeholder="Bezeichnung" value="">
+		<input type="text" class="oc_calendar_change" name="oc_calendar_user" placeholder="Username" value="">
+		<input type="text" class="oc_calendar_change" name="oc_calendar_password" placeholder="Passwort" value="">
+		<input type="text" class="oc_calendar_change" name="oc_calendar_url" placeholder="url<?php echo _('oc_importer_url');?>" value=""/>
+	</div>
 
 </form>
 <div class="block__add" id="oc_calendar_import__edit">
